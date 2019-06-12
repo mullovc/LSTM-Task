280b4435672a4d5ae594943976d3433562305c90
Carlos Mullov <mullovc@hotmail.de>
2019-06-12 02:36:15 +0200

    simplify LSTM layer backward pass

--- modules/lstm_layer.py
+++ modules/lstm_layer.py
@@ -29,49 +29,36 @@ class LSTMLayer(Module):
 
     def backward(self, activations, dLdOut):
         '''
-        Differentiate layer output w.r.t. weights and w.r.t. inputs.
+        Differentiate layer outputs w.r.t. weights and w.r.t. inputs.
         '''
         tanh_c_out, o_t, g_t, f_t, i_t, h_in, c_in, x_t = activations
-        dLdhout, dLdcout = dLdOut
-
-        W_ii, W_if, W_ig, W_io = np.split(self.W_ih, 4)
-        W_hi, W_hf, W_hg, W_ho = np.split(self.W_hh, 4)
-
+        dLdhout, dLdcout_part = dLdOut
 
         dhoutdo = tanh_c_out
-        #dhoutdcout = o_t * (1 - np.square(tanh_c_out)) # + dL/dc_{t+1} * f_{t+1} (= dc_{t+1}/dc_{t})
-        dhoutdcout = o_t * (1 - np.square(tanh_c_out)) + dLdcout
+        # dLdcout_part = dL/dc_{t+1} * dc_{t+1}/dc_{t} = dL/dc_{t+1} * f_{t+1}
+        dLdcout = dLdhout * o_t * (1 - np.square(tanh_c_out)) + dLdcout_part
 
-        dody = o_t * (1 - o_t)
         didy = i_t * (1 - i_t)
-        dgdy = 1 - np.square(g_t)
         dfdy = f_t * (1 - f_t)
+        dgdy = 1 - np.square(g_t)
+        dody = o_t * (1 - o_t)
+
+        dcoutdf = c_in
+        dcoutdg = i_t
+        dcoutdi = g_t
+
+        dLdy = np.concatenate([dLdcout * dcoutdi * didy,
+                               dLdcout * dcoutdf * dfdy,
+                               dLdcout * dcoutdg * dgdy,
+                               dLdhout * dhoutdo * dody], 0)
+
+        dLdW_ih = np.dot(dLdy, x_t.transpose())
+        dLdW_hh = np.dot(dLdy, h_in.transpose())
+
+        dLdhin = np.dot(self.W_hh.transpose(), dLdy)
+        dLdx   = np.dot(self.W_ih.transpose(), dLdy)
 
         dcoutdcin = f_t
-        dcoutdf   = c_in
-        dcoutdg   = i_t
-        dcoutdi   = g_t
-
-        dLdW_io = np.dot(dLdhout * dhoutdo * dody, x_t.transpose())
-        dLdW_ho = np.dot(dLdhout * dhoutdo * dody, h_in.transpose())
-        dLdW_ii = np.dot(dLdhout * dhoutdcout * dcoutdi * didy, x_t.transpose())
-        dLdW_hi = np.dot(dLdhout * dhoutdcout * dcoutdi * didy, h_in.transpose())
-        dLdW_if = np.dot(dLdhout * dhoutdcout * dcoutdf * dfdy, x_t.transpose())
-        dLdW_hf = np.dot(dLdhout * dhoutdcout * dcoutdf * dfdy, h_in.transpose())
-        dLdW_ig = np.dot(dLdhout * dhoutdcout * dcoutdg * dgdy, x_t.transpose())
-        dLdW_hg = np.dot(dLdhout * dhoutdcout * dcoutdg * dgdy, h_in.transpose())
-
-        dLdhin  = np.dot(W_ho.transpose(), dLdhout * dhoutdo * dody)
-        dLdx    = np.dot(W_io.transpose(), dLdhout * dhoutdo * dody)
-        dLdhin += np.dot(W_hi.transpose(), dLdhout * dhoutdcout * dcoutdi * didy)
-        dLdx   += np.dot(W_ii.transpose(), dLdhout * dhoutdcout * dcoutdi * didy)
-        dLdhin += np.dot(W_hf.transpose(), dLdhout * dhoutdcout * dcoutdf * dfdy)
-        dLdx   += np.dot(W_if.transpose(), dLdhout * dhoutdcout * dcoutdf * dfdy)
-        dLdhin += np.dot(W_hg.transpose(), dLdhout * dhoutdcout * dcoutdg * dgdy)
-        dLdx   += np.dot(W_ig.transpose(), dLdhout * dhoutdcout * dcoutdg * dgdy)
-
         dLdcin = dLdcout * dcoutdcin
 
-        return (dLdhin, dLdcin), \
-               (dLdW_ii, dLdW_if, dLdW_ig, dLdW_io, \
-                dLdW_hi, dLdW_hf, dLdW_hg, dLdW_ho)
+        return (dLdhin, dLdcin), (dLdW_ih, dLdW_hh)
