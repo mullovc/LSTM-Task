15537289e89c416ed2f84e31d7aaa7337bd47529
Carlos Mullov <mullovc@hotmail.de>
2019-06-12 17:49:14 +0200

    implement LSTM backward pass for sequences

--- modules/LSTM.py
+++ modules/LSTM.py
@@ -11,22 +11,26 @@ class LSTM(Module):
 
     def forward(self, X, (h_n, c_n)):
         seq_len = X.shape[1]
+
+        # gates = []
         hiddens = []
-        gates   = []
+        activations = []
         for i in range(seq_len):
             x_t = X[:, i, :].transpose()
 
-            h_n, c_n = self.lstm_layer(x_t, (h_n, c_n))
+            h_n, c_n, act_t = self.lstm_layer(x_t, (h_n, c_n))
 
+            # gates.append(c_n)
             hiddens.append(h_n)
-            gates.append(c_n)
+            activations.append(act_t)
 
-        self.gates = np.stack(gates).transpose([2, 0, 1])
-        self.hiddens = np.stack(hiddens).transpose([2, 0, 1])
+        # gates = np.stack(gates).transpose([2, 0, 1])
+        hiddens = np.stack(hiddens).transpose([2, 0, 1])
+        # activations = np.stack(activations).transpose([2, 0, 1])
 
-        return self.hiddens, (h_n, c_n)
+        return hiddens, (h_n, c_n), activations
 
-    def backward(self, out):
+    def backward(self, activations, dLdOut):
         '''
         Differentiate error w.r.t. weights and w.r.t. input.
         Steps:
@@ -34,4 +38,16 @@ class LSTM(Module):
                 - delegate gradient computation to LSTMLayer
             2. apply chain rule: accumulate "outer" gradient
         '''
-        raise NotImplementedError
+        dLdOut_t = (dLdOut, 0)
+        dW_ih = []
+        dW_hh = []
+        for act_t in activations[::-1]:
+            dLdOut_t, (dW_iht, dW_hht) = self.lstm_layer.backward(act_t, dLdOut_t)
+
+            dW_ih.append(dW_iht)
+            dW_hh.append(dW_hht)
+
+        dW_ih = np.mean(dW_ih, 0)
+        dW_hh = np.mean(dW_hh, 0)
+
+        return dW_ih, dW_hh
