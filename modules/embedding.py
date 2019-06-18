3b642366b2ef2eecc9fa8ffe247f7ac5e1026c70
Carlos Mullov <mullovc@hotmail.de>
2019-06-18 04:29:53 +0200

    add embedding module

---
+++ modules/embedding.py
@@ -0,0 +1,30 @@
+import numpy as np
+from modules.module import Module
+from modules.LSTM import LSTM
+
+class Embedding(Module):
+    def __init__(self, input_size, embedding_size):
+        super(Embedding, self).__init__()
+        self.input_size = input_size
+        self.embedding_size = embedding_size
+
+        std = 1.0 / np.sqrt(input_size + embedding_size)
+
+        self.W = np.random.normal(0, std, [input_size, embedding_size])
+
+    def forward(self, x):
+        shape = x.shape + (self.embedding_size,)
+        x_flat = x.flatten()
+
+        out = self.W[x_flat]
+
+        self.x = (x_flat,)
+        return out.reshape(shape)
+
+    def backward(self, dLdOut):
+        x, = self.x
+        # flatten all extra dimensions into one batch dimension
+        dLdOut2D = dLdOut.reshape([-1,dLdOut.shape[-1]])
+        self.gradients["W"][x] += dLdOut2D
+
+        # doesn't return input gradient
